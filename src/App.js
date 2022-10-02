c00cc7a39e9b8f9bfa6a3d721a416762444a3308
Victor Tsokonov <victorcodingacc@gmail.com>
2022-10-02 15:38:30 +0300

    add styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Fragment, useState } from "react";
 function App() {
   const [image, setImage] = useState("");
   const [explanation, setExplanation] = useState("");
+
   async function onClickListener() {
     let response = await fetch(
       `https://api.nasa.gov/planetary/apod?api_key=bJqJCfjviictVxF6eyL1MGwBpSYKSJo4mXPZtkeK`
@@ -16,21 +17,24 @@ function App() {
 
   console.log(image);
   return (
-    <Fragment>
+    <div className="container">
       <h1>ArtFinder Prototype</h1>
-      <button onClick={onClickListener}>Click Me!</button>
+      <div className="form">
+        <input type="text" />
+        <button onClick={onClickListener}>Find</button>
+      </div>
       {image && explanation ? (
         <Fragment>
           <img
             src={image}
+            width="1800px"
+            height="1000px"
             alt="No Image Found :("
-            width="1920px"
-            height="1080px"
           />
           <p>{explanation}</p>
         </Fragment>
       ) : null}
-    </Fragment>
+    </div>
   );
 }
 
